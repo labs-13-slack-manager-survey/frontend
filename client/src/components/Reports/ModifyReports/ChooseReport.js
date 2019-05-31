d75138b80198449b119040bedd92d7fdf2df974b
Mckay <mckaywrigley@gmail.com>
2019-05-31 10:49:02 -0700

    adds radio button option for choosing if manager questions should be added in standup form

---
+++ client/src/components/Reports/ModifyReports/ChooseReport.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+import { Button } from "@material-ui/core";
+
+class ChooseReport extends Component {
+  render() {
+    return <div />;
+  }
+}
+
+export default ChooseReport;
