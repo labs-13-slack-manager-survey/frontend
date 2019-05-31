c00fb03179ba3253bd1f6b6d8063e5efc85a79f7
Mckay <mckaywrigley@gmail.com>
2019-05-31 11:08:25 -0700

    creates initial ChooseReport component

--- client/src/components/Reports/ModifyReports/ChooseReport.js
+++ client/src/components/Reports/ModifyReports/ChooseReport.js
@@ -1,11 +1,43 @@
 import React, { Component } from "react";
 
-import { Button } from "@material-ui/core";
+import { withStyles, Card, Button, Divider } from "@material-ui/core";
+
+const styles = theme => ({
+  container: {
+    display: "flex",
+    flexWrap: "wrap"
+  },
+  textField: {
+    marginLeft: 0,
+    marginRight: theme.spacing.unit,
+    width: 200
+  },
+  menu: {
+    width: 200
+  }
+});
 
 class ChooseReport extends Component {
   render() {
-    return <div />;
+    return (
+      <Card raised={true} className="create-report">
+        <section className="schedule-card-content">
+          <h3 className="schedule-title">What report do you want to create?</h3>
+          <Divider className="divider" variant="fullWidth" />
+          <section>
+            <Button variant="outlined" style={{ marginTop: "20px" }}>
+              Create Standup Survey
+            </Button>
+          </section>
+          <section>
+            <Button variant="outlined" style={{ marginTop: "20px" }}>
+              Create Sentiment Poll
+            </Button>
+          </section>
+        </section>
+      </Card>
+    );
   }
 }
 
-export default ChooseReport;
+export default withStyles(styles)(ChooseReport);
