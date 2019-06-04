ad7fd7f1160ada146b642a11c23b001e79cd5883
Erica <eyufanchen@gmail.com>
2019-06-04 15:37:40 -0700

    Added range slider for sentiment polls

--- client/src/components/Reports/MemberReports/ReportInput.js
+++ client/src/components/Reports/MemberReports/ReportInput.js
@@ -1,30 +1,78 @@
 import React from 'react';
 import TextField from '@material-ui/core/TextField';
 import Slider from '@material-ui/lab/Slider';
+import { withStyles} from '@material-ui/core/styles'
+import { fade } from '@material-ui/core/styles/colorManipulator';
+import './Report_Input.css';
 
 // this component handles the response inputs for individual questions
 // when a user is filling in a report
 // parent component = MemberResponseForm.js
 
+//styles for slider
+
+
+const StyledSlider = withStyles({
+	thumb: {
+		height: 24,
+		width: 24, 
+		backgroundColor: '#F67B28',
+		border: `3px solid #fff`,
+		'&$focused, &:hover': {
+		boxShadow: `0px 0px 0px ${8}px ${fade('#de235b', 0.16)}`,
+		},
+		'&$activated': {
+		boxShadow: `0px 0px 0px ${6}px ${fade('#de235b', 0.16)}`,
+		},
+		'&$jumped': {
+		boxShadow: `0px 0px 0px ${2}px ${fade('#de235b', 0.16)}`,
+		},
+	},
+	track: {
+		backgroundColor: '#FEBA47',
+		height: 8,
+	  },
+	  trackAfter: {
+		backgroundColor: '#d0d7dc',
+	  },
+})(Slider);
 
 class ReportInput extends React.Component {
-
-	checkSentiment = () => {
-		console.log(this.props.isSentiment);
+	//starting value for the poll
+	state = {
+		sentimentVal: 3, 
 	}
+	
+	handleChange = (event, sentimentVal) => {
+		this.setState({ sentimentVal });
+		console.log(this.state.sentimentVal)
+	};
 
 	render () {
-		return (
+
+		return (		
 			<div className="member-report-input">
 				<h4>{this.props.question}</h4>
 				{this.props.isSentiment ? ( 
-				<Slider
-
-					min={0}
-					max={6}
-					step={1}
-					onChange={this.handleChange}
-        			/>)  : 
+					<>
+					<StyledSlider
+						className = "slider"
+						value = {this.state.sentimentVal} 
+						min={1}
+						max={5}
+						step={1}
+						onChange={this.handleChange}
+						/> 
+					<div className="slider-label">
+						<p>1</p>
+						<p>2</p>
+						<p>3</p>
+						<p>4</p>	
+						<p>5</p>
+					</div>	
+					</>
+						)
+					 : 
 				(
 					<TextField
 						fullWidth={true}
@@ -36,10 +84,10 @@ class ReportInput extends React.Component {
 						variant="outlined"
 					/>
 				)} 
-				<button onClick={this.checkSentiment} >isSentiment</button> 
 			</div>
 		);
 	}
 };
+
 
 export default ReportInput;
