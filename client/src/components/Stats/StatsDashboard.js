149972a44828fafb26a4e77e9725fd02e279c62e
Mckay <mckaywrigley@gmail.com>
2019-06-05 09:17:08 -0700

    adds in route for StatsDashbaord

---
+++ client/src/components/Stats/StatsDashboard.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import { Route } from "react-router-dom";
+
+class StatsDashboard extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Stats</h2>
+      </div>
+    );
+  }
+}
+
+export default StatsDashboard;
