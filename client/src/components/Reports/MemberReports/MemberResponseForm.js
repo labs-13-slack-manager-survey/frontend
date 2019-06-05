0b25acdee939ee9cd0379657619dedfc67548654
Ben <cbtsao47@gmail.com>
2019-06-04 23:36:23 -0500

    convert ' to ''

--- client/src/components/Reports/MemberReports/MemberResponseForm.js
+++ client/src/components/Reports/MemberReports/MemberResponseForm.js
@@ -1,99 +1,105 @@
-import React, { Component } from 'react';
-import { axiosWithAuth, baseURL } from '../../../config/axiosWithAuth';
+import React, { Component } from "react";
+import { axiosWithAuth, baseURL } from "../../../config/axiosWithAuth";
 
-import ReportInput from './ReportInput';
+import ReportInput from "./ReportInput";
 
 // style imports
-import Button from '@material-ui/core/Button';
-import './MemberResponseForm.css';
+import Button from "@material-ui/core/Button";
+import "./MemberResponseForm.css";
 
 class MemberResponseForm extends Component {
-	state = {
-		clientInfo: '',
-		reportName: '',
-		reportMessage: '',
-		questions: [],
-		isSentiment: false,
-	};
+  state = {
+    clientInfo: "",
+    reportName: "",
+    reportMessage: "",
+    questions: [],
+    isSentiment: false
+  };
 
-	render() {
-		return this.state.clientInfo.length > 0 ? (
-			<>
-				<div>{this.state.clientInfo}</div>
-			</>
-		) : (
-			<div>
-				<h1 className="member-form-title">{this.state.reportName}</h1>
-				<p className="member-form-subtitle">{this.state.reportMessage}</p>
-				{this.state.questions.map((q, i) => (
-					<ReportInput
-						question={q.question}
-						response={q.response}
-						handleChange={this.handleChange}
-						key={i}
-						isSentiment={this.state.isSentiment}
-					/>
-				))}
-				<Button
-					style={{ display: 'block', margin: 'auto', marginTop: '30px', marginBottom: '30px' }}
-					variant="outlined"
-					color="primary"
-					onClick={this.submitReport}
-				>
-					Submit Report
-				</Button>
-			</div>
-		);
-	}
+  render() {
+    return this.state.clientInfo.length > 0 ? (
+      <>
+        <div>{this.state.clientInfo}</div>
+      </>
+    ) : (
+      <div>
+        <h1 className="member-form-title">{this.state.reportName}</h1>
+        <p className="member-form-subtitle">{this.state.reportMessage}</p>
+        {this.state.questions.map((q, i) => (
+          <ReportInput
+            question={q.question}
+            response={q.response}
+            handleChange={this.handleChange}
+            key={i}
+            isSentiment={this.state.isSentiment}
+          />
+        ))}
+        <Button
+          style={{
+            display: "block",
+            margin: "auto",
+            marginTop: "30px",
+            marginBottom: "30px"
+          }}
+          variant="outlined"
+          color="primary"
+          onClick={this.submitReport}
+        >
+          Submit Report
+        </Button>
+      </div>
+    );
+  }
 
-	componentDidMount() {
-		const endpoint = `${baseURL}/reports/${this.props.match.params.reportId}`;
-		axiosWithAuth()
-			.get(endpoint)
-			.then(res => {
-				console.log(res.data.report);
-				const { reportName, message, questions, isSentiment } = res.data.report;
-				this.setState({
-					reportName,
-					reportMessage: message,
-					questions: questions.map(q => ({
-						question: q,
-						response: ''
-					})), 
-					isSentiment: isSentiment, 
-				});
-			})
-			.catch(err => console.log(err));
-	}
+  componentDidMount() {
+    const endpoint = `${baseURL}/reports/${this.props.match.params.reportId}`;
+    axiosWithAuth()
+      .get(endpoint)
+      .then(res => {
+        console.log(res.data.report);
+        const { reportName, message, questions, isSentiment } = res.data.report;
+        this.setState({
+          reportName,
+          reportMessage: message,
+          questions: questions.map(q => ({
+            question: q,
+            response: ""
+          })),
+          isSentiment: isSentiment
+        });
+      })
+      .catch(err => console.log(err));
+  }
 
-	handleChange = (e, question) => {
-		const qObj = { question, response: e.target.value };
-		this.setState(prevState => ({
-			...prevState,
-			questions: prevState.questions.map(q =>
-				q.question !== question ? q : qObj
-			)
-		}));
-	};
+  handleChange = (e, question) => {
+    const qObj = { question, response: e.target.value };
+    this.setState(prevState => ({
+      ...prevState,
+      questions: prevState.questions.map(q =>
+        q.question !== question ? q : qObj
+      )
+    }));
+  };
 
-	submitReport = () => {
-		const endpoint = `${baseURL}/responses/${this.props.match.params.reportId}`;
-		axiosWithAuth()
-			.post(endpoint, this.state.questions)
-			.then(res => {
-				this.props.updateWithUserResponse(res);
-				this.setState(prevState => ({
-					...prevState,
-					questions: prevState.questions.map(q => ({
-						question: q.question,
-						response: ''
-					}))
-				}));
-			})
-			.catch(err => {
-				console.log(err.response.data);
-			});
-	};
+  submitReport = () => {
+    const endpoint = `${baseURL}/responses/${this.props.match.params.reportId}`;
+
+    axiosWithAuth()
+      .post(endpoint, this.state.questions)
+      .then(res => {
+        this.props.updateWithUserResponse(res);
+        this.setState(prevState => ({
+          ...prevState,
+          questions: prevState.questions.map(q => ({
+            question: q.question,
+            response: ""
+          }))
+        }));
+      })
+      .catch(err => {
+        console.log(err.response.data);
+      });
+  };
 }
 
 export default MemberResponseForm;
