ce294e1f11f3f3742e8f757e7ff2c9468a6dd9e2
Taylor <wwwaaavvveeerrr@gmail.com>
2019-06-05 16:36:46 -0400

    survey questions fixed

--- client/src/components/Reports/ModifyReports/CreateReport.js
+++ client/src/components/Reports/ModifyReports/CreateReport.js
@@ -28,7 +28,7 @@ Menu
 import MenuItem from "@material-ui/core/MenuItem";
 import { TimePicker } from "material-ui-pickers";
 import PopupState, { bindTrigger, bindMenu } from 'material-ui-popup-state';
-
+import AddIcon from '@material-ui/icons/Add';
 
 
 import "./Report.css";
@@ -263,6 +263,36 @@ aQuestion = e =>{
   e.preventDefault();
   this.setState({aQuestion:e.target.value})
 }
+
+enterQuestionsHandler = e => {
+  e.preventDefault();
+  const code = e.keyCode || e.which;
+  if (code === 13) {
+    this.setState(prevState => ({
+      questions: [...prevState.questions, this.state.question],
+      question: ""
+    }));
+  } else {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  }
+};
+
+questionsHandler = e => {
+  e.preventDefault();
+  this.setState(prevState => ({
+    questions: [...prevState.questions, this.state.question],
+    question: ""
+  }));
+};
+
+removeQuestion = (e, question) => {
+  e.preventDefault();
+  this.setState(prevState => ({
+    questions: prevState.questions.filter(q => q !== question)
+  }));
+};
 
 
 
@@ -579,18 +609,57 @@ render() {
               />
             </section>
           </section>
-        </Card>
+</Card>
 
-        <Card raised={true} className="schedule-card">
-          <section className="schedule-card-content">
-            <h3 className="schedule-title">Standup Questions</h3>
-            <Divider className="divider" variant="fullWidth" />
-            <section>
-              {/*----------This will change display based on if user checked manager questions------- */}
-              {this.RenderSurveyQuestions()}
+<Card raised={true} className="schedule-card">
+            <section className="schedule-card-content">
+              <h3 className="schedule-title">Standup Questions</h3>
+              <Divider className="divider" variant="fullWidth" />
+              <section>
+                {this.state.questions.map(question => (
+                  <article className="question-flex" key={question}>
+                    <p className="question">{question}</p>
+                    <Fab
+                      size="small"
+                      color="secondary"
+                      onClick={e => this.removeQuestion(e, question)}
+                    >
+                      <Icon>delete_icon</Icon>
+                    </Fab>
+                  </article>
+                ))}
+              </section>
+              <section className="enter-question">
+                <FormControl className="input-field" required>
+                  <InputLabel htmlFor="report-question">
+                    Enter a question...
+                  </InputLabel>
+                  <Input
+                    id="report-question"
+                    required
+                    className="input-field"
+                    type="text"
+                    name="question"
+                    value={this.state.question}
+                    onChange={this.enterQuestionsHandler}
+                  />
+                </FormControl>
+                <Fab
+                  size="small"
+                  style={{ display: "block", margin: "10px 0" }}
+                  color="primary"
+                  onClick={this.questionsHandler}
+                  disabled={this.state.question.length === 0 ? true : false}
+                  type="submit"
+                >
+                  <AddIcon />
+                </Fab>
+              </section>
             </section>
-          </section>
-        </Card>
+          </Card>
+
+
+      
 
         <Button
           style={{ display: "block", marginTop: "30px" }}
