61d5990f2506f17d20553956748943898febb635
Mckay <mckaywrigley@gmail.com>
2019-06-06 13:51:38 -0700

    changes calendar to reac-tdates

--- client/src/components/Stats/PollCalendar.js
+++ client/src/components/Stats/PollCalendar.js
@@ -1,10 +1,17 @@
 import React, { Component } from "react";
+import "react-dates/initialize";
+import "react-dates/lib/css/_datepicker.css";
+import { DayPickerSingleDateController } from "react-dates";
+import { isSameDay } from "date-fns";
+import parseISO from "date-fns/parseISO";
 
 class PollCalendar extends Component {
   render() {
     return (
       <div style={{ marginTop: "20px" }}>
-        <h2>Calendar</h2>
+        <div className="report-results-date-picker">
+          <DayPickerSingleDateController numberOfMonths={1} noBorder={true} />
+        </div>
       </div>
     );
   }
