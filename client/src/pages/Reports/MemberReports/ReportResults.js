4be82537569430ec62a7ee7ee74920ee5f062903
Erica Chen <andrew@Andrews-MacBook-Pro.local>
2019-06-11 17:11:18 -0700

    redesigned the individual report result page

--- client/src/pages/Reports/MemberReports/ReportResults.js
+++ client/src/pages/Reports/MemberReports/ReportResults.js
@@ -10,6 +10,8 @@ import PageTitle from '../../../components/PageTitle';
 import Slider from "@material-ui/lab/Slider";
 import { withStyles } from "@material-ui/core/styles";
 import { fade } from "@material-ui/core/styles/colorManipulator";
+import CircleProgress from '../../../components/circleProgress.js';
+
 
 
 
@@ -58,19 +60,19 @@ class ReportResults extends Component {
     };
 
     return (
-      <div className = "view">
+      <div className="dashboard-view">
+
+
+      
+      <main className="view">
       <PageTitle 
       title = "Report"
       />
-
-      
-      <main className="report-results-container">
-        <div className="report-results-container-backButton">
+                      <div className="report-results-container-backButton">
           <Fab onClick={() => this.props.history.goBack()} color="default">
             <Icon>arrow_back</Icon>
           </Fab>
         </div>
-
         {/* <section className="report-results-aside">
           {this.state.filteredResponse.length > 0 ||
           this.state.completed === true ? (
@@ -116,92 +118,107 @@ class ReportResults extends Component {
             />
           </Card>
         </section> */}
-      </main>
 
 
-      <section className="report-results-feed">
-          {this.state.responses.map(
-            batch =>
-              batch.responses.length > 0 && (
-                <div key={batch.date}>
-                  <h3 className="report-results-feed-date">
-                    {new Date(batch.date)
-                      .toLocaleDateString("en-US", options)
-                      .replace(",", "")}
-                  </h3>
-                  {batch.responses.map(response => (
+          <section className="report-results-feed">
+              {this.state.responses.map(
+                batch =>
+                  batch.responses.length > 0 && (
+                    <div key={batch.date}>
+                      <h3 className="report-results-feed-date">
+                        {new Date(batch.date)
+                          .toLocaleDateString("en-US", options)
+                          .replace(",", "")}
+                      </h3>
+                      {batch.responses.map(response => (
 
-                    <div key={response.userId}>
-                      <div className="response-container">
-                        <div className = "user-info">
-                            <div className = "month-day">
-                            {batch.date}
+                        <div key={response.userId}>
+                          <div className="response-container">
+                            <div className = "user-info">
+                                <div className = "month-day">
+                                {batch.date}
+                                </div>
+                              {/* <img
+                                className="response-container-image"
+                                src={response.profilePic}
+                                alt={response.fullName}
+                              /> */}
+                              <div className="response-container-main-name">
+                                  {response.fullName}
+                              </div>
                             </div>
-                          {/* <img
-                            className="response-container-image"
-                            src={response.profilePic}
-                            alt={response.fullName}
-                          /> */}
-                          <div className="response-container-main-name">
-                              {response.fullName}
+
+
+                            <div className="response-container-main">
+                              <div className = "vertical-timeline" />
+                              <div className = "response-content">
+                                  {response.questions.map(
+                                    ({ question, answer, id, sentimentRange }) => (
+                                      <div key={id}>
+                                        <div className="response-container-main-question">
+                                          {question}
+                                        </div>
+                                        {sentimentRange && 
+                                        <>
+                                            <StyledSlider
+                                              className="slider"
+                                              value={sentimentRange}
+                                              min={1}
+                                              max={5}
+                                              step={1}
+                                            />
+                                              <div className="slider-label">
+                                                <p className={sentimentRange !=1 ? "deselected" : null}>1</p>
+                                                <p className={sentimentRange !=2 ? "deselected" : null}>2</p>
+                                                <p className={sentimentRange !=3 ? "deselected" : null}>3</p>
+                                                <p className={sentimentRange !=4 ? "deselected" : null}>4</p>
+                                                <p className={sentimentRange !=5 ? "deselected" : null}>5</p>
+                                              </div>
+                                          </>
+                                        }
+                                        <p className="response-container-main-answer">
+                                          {/* {sentimentRange && <div className="view-comments">Comments:</div>} */}
+                                          <div className={sentimentRange ?  "sentiment-comment":null}>{answer}</div>
+                                        </p>
+                                      </div>
+                                    )
+                                  )}
+                              </div>
+
+                            </div>
+
+
                           </div>
                         </div>
-
-
-                        <div className="response-container-main">
-                          <div className = "vertical-timeline" />
-                          <div className = "response-content">
-                              {response.questions.map(
-                                ({ question, answer, id, sentimentRange }) => (
-                                  <div key={id}>
-                                    <div className="response-container-main-question">
-                                      {question}
-                                    </div>
-                                    {sentimentRange && 
-                                    <>
-                                        <StyledSlider
-                                          className="slider"
-                                          value={sentimentRange}
-                                          min={1}
-                                          max={5}
-                                          step={1}
-                                        />
-                                          <div className="slider-label">
-                                            <p className={sentimentRange !=1 ? "deselected" : null}>1</p>
-                                            <p className={sentimentRange !=2 ? "deselected" : null}>2</p>
-                                            <p className={sentimentRange !=3 ? "deselected" : null}>3</p>
-                                            <p className={sentimentRange !=4 ? "deselected" : null}>4</p>
-                                            <p className={sentimentRange !=5 ? "deselected" : null}>5</p>
-                                          </div>
-                                      </>
-                                    }
-                                    <p className="response-container-main-answer">
-                                      {sentimentRange && <div>Comments:</div>}
-                                      {answer}
-                                    </p>
-                                  </div>
-                                )
-                              )}
-                          </div>
-
-                        </div>
-
-
-                      </div>
+                        
+                      ))}
                     </div>
-                    
-                  ))}
-                </div>
-              )
-          )}
-        </section>
+                  )
+              )}
+            </section>
+        </main>  
+        <div className = "sidebar">
+          <CircleProgress 
+          title = "Today's Polls"
+          percentComplete = '0.6'/>
 
+          {/* <PollCalendar /> */}
+        </div>
+                          
       </div>
     );
   }
 
   componentDidMount() {
     const userId = jwt_decode(localStorage.getItem("token")).subject;
+
+    // axiosWithAuth() 
+    //   .get(`${baseURL}/reports/submissionRate/${this.props.match.params.reportId}`)
+    //   .then(res=>{
+    //     console.log(res);
+    //   })
+
+      // .catch(err => console.log(err))
     axiosWithAuth()
       .get(`${baseURL}/responses/${this.props.match.params.reportId}`)
       .then(res => {
