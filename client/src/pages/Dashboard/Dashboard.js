27a0e20543e08288c79abd1c62ad2e2a715c6725
curtis <chubbard022@gmail.com>
2019-06-16 11:25:24 -0700

    delete excesive console.logs and unused percent wheele in myTeam page

--- client/src/pages/Dashboard/Dashboard.js
+++ client/src/pages/Dashboard/Dashboard.js
@@ -74,7 +74,6 @@ export class Dashboard extends Component {
       axiosWithAuth()
       .get(`${baseURL}/reports/submissionRate`)
       .then(res => {
-        console.log(res.data.totalResponses);
         this.setState({
           totalResponses: res.data.totalResponses,
         })
