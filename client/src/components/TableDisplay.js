3605c2163ebab773be6a7278d416d809ec49f9a4
Erica Chen <andrew@Andrews-MacBook-Pro.local>
2019-06-19 08:55:40 -0700

    allowed users to click on each report in the dashboard

--- client/src/components/TableDisplay.js
+++ client/src/components/TableDisplay.js
@@ -6,10 +6,10 @@ import edit from '../images/icons/edit.png';
 import trashCan from '../images/icons/trash.png';
 import './tableDisplay.css';
 import {
-    Dialog,
+	Dialog,
     DialogTitle,
     Slide,
-    Button
+    Button,
 } from '@material-ui/core';
 
 const week = [
@@ -25,14 +25,14 @@ const week = [
 // time refactor for api call
 
 function Transition(props) {
-    return <Slide direction="up" {...props} />;
+	return <Slide direction="up" {...props} />;
 }
 
 class TableDisplay extends React.Component {
     state = {
         dialogOpen: false,
         totalResponse: 0
-    }
+	}
     render() {
         const time = this.props.report.scheduleTime.split(':');
         let timeStr = `${time[0]}:${time[1]}am`;
@@ -45,8 +45,11 @@ class TableDisplay extends React.Component {
             <div className="table-display">
 
             <div className = "content">
-                <div className = "column1">{this.props.content1} </div>
-
+            <Link
+				to={`/slackr/dashboard/reports/${this.props.report.id}`}
+				style={{ textDecoration: 'none' }} className = "column1">
+                <div>{this.props.content1}</div>
+                </Link>
                 <div className = "date">{dateString.getMonth()+1}/{dateString.getDate()}/{dateString.getFullYear()}</div>
 
                 <div className = "schedule-time">
@@ -77,40 +80,36 @@ class TableDisplay extends React.Component {
 
                 <div className = "action-icons">
                     <Link
-                        to={`/slackr/dashboard/reports/${this.props.report.id}/edit`}
-                        id={this.props.role !== 'admin' ? 'display-link' : ''}
-                    >
+                        to={`/slackr/dashboard/reports/${this.props.report.id}/edit`}
+                        id={this.props.role !== 'admin' ? 'display-link' : ''}
+                    >
                         <img className ="action" src={edit} />
                     </Link>
 
                     <img onClick={() => this.setState({dialogOpen: true})} id={this.props.role !== 'admin' ? 'display-link' : ''} className ="action" src={trashCan} />
 
                     <Dialog
-                        open={this.state.dialogOpen}
-                        TransitionComponent={Transition}
-                        keepMounted
-                        onClose={this.props.clearError}
-                        aria-labelledby="alert-dialog-slide-title"
-                        aria-describedby="alert-dialog-slide-description"
-                    >
-                        <DialogTitle id="alert-dialog-slide-title">
-                            Are you sure you'd like to archive this report? 
-                        </DialogTitle>
-
-                        <button onClick={() => this.props.archiveReport(this.props.report.id)}>
-                            Yes
-                        </button>
-                        <button onClick={() => {
-                            this.setState({dialogOpen: false})
-                            }}>No</button>
-                    </Dialog>
-
+                        open={this.state.dialogOpen}
+                        TransitionComponent={Transition}
+                        keepMounted
+                        onClose={this.props.clearError}
+                        aria-labelledby="alert-dialog-slide-title"
+                        aria-describedby="alert-dialog-slide-description"
+                    >
+                        <DialogTitle id="alert-dialog-slide-title">
+                            Are you sure you'd like to archive this report? 
+                        </DialogTitle>
+                        <button onClick={() => this.props.archiveReport(this.props.report.id)}>
+                            Yes
+                        </button>
+                        <button onClick={() => {
+                            this.setState({dialogOpen: false})
+                            }}>No</button>
+                    </Dialog>
                 </div>
-
             </div>
                             
             </div> 
         )}
 };
-
 export default TableDisplay; 
