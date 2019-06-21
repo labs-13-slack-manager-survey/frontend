40dd3025de08ddf1221f66a626e02cdd0a1f677e
Ben <cbtsao47@gmail.com>
2019-06-21 04:17:37 -0500

    remove logs

--- client/src/components/InviteUser.js
+++ client/src/components/InviteUser.js
@@ -1,122 +1,118 @@
-import React from 'react';
+import React from "react";
 import jwt_decode from "jwt-decode";
 import { axiosWithAuth, baseURL } from "../config/axiosWithAuth.js";
 import {
-	TextField,
-	Button,
-	Dialog,
-	DialogTitle,
-	Slide,
-	Input
-} from '@material-ui/core';
+  TextField,
+  Button,
+  Dialog,
+  DialogTitle,
+  Slide,
+  Input
+} from "@material-ui/core";
 
-import './inviteUser.css';
+import "./inviteUser.css";
 
 // component to invite user to team (only admin access - BE preventions also)
 
 function Transition(props) {
-	return <Slide direction="up" {...props} />;
+  return <Slide direction="up" {...props} />;
 }
 
 class InviteUser extends React.Component {
-	state = {
-		users: [],
-		message: '',
-		joinCode: '',
-		modal: false, 
-	}
+  state = {
+    users: [],
+    message: "",
+    joinCode: "",
+    modal: false
+  };
 
-	componentDidMount() {
-		// get user's joinCode from token and setState accordingly. Necessary to invite new team members.
-		const joinCode = jwt_decode(localStorage.getItem("token")).joinCode;
-		console.log(joinCode);
-		console.log(this.state.users.length)
-		this.setState({
-		  joinCode: joinCode
-		});
-		axiosWithAuth()
-		  .get(`${baseURL}/users/team`)
-		  .then(res => {
-			console.log(res);
-			this.setState({ users: res.data.users });
-	
-			if (this.state.users.length > 0) {
-			  this.setState({ isLoading: false });
-			}
-		  })
-		  .catch(err => console.log(err));
-	  }
+  componentDidMount() {
+    // get user's joinCode from token and setState accordingly. Necessary to invite new team members.
+    const joinCode = jwt_decode(localStorage.getItem("token")).joinCode;
+    this.setState({
+      joinCode: joinCode
+    });
+    axiosWithAuth()
+      .get(`${baseURL}/users/team`)
+      .then(res => {
+        this.setState({ users: res.data.users });
 
-	  addUser = e => {
-		e.preventDefault();
-		//create mailObject to post to sendgrid API
-		const mailObject = {
-		  email: this.state.newMemberEmail,
-		  joinCode: this.state.joinCode
-		};
-		console.log(mailObject);
-		//sendgrid endpoint on our back end
-		const endpoint = `${baseURL}/email`;
-	
-		axiosWithAuth()
-		  .post(endpoint, mailObject)
-		  .then(res => {
-			console.log(res);
-			this.setState({ message: "Email sent!", modal: true });
-		  })
-		  .catch(err => {
-			console.log(err);
-			this.setState({
-			  message:
-				"There was an issue sending the email, please email your new team member manually.",
-			  modal: true
-			});
-		  });
-	  };
-	
-	  changeHandler = e => {
-		this.setState({ newMemberEmail: e.target.value });
-	  };
-	  clearMessage = () => {
-		this.setState({ message: "", modal: false });
-	  };
-	  
-	render () {
-		return (
-			<div className="inviteUserBox">
-				<div className="invite">Invite a new team member</div>
-				<form onSubmit={this.addUser} className="inviteUser">
-					<Input
-						id="outlined-email-input"
-						label="Email"
-						type="email"
-						onChange={this.changeHandler}
-						name="newMemberEmail"
-						autoComplete="email"
-						margin="normal"
-						variant="outlined"
-						placeholder="add email"
-					/>
-					<br />
-					<Button variant="outlined" type="submit" onClick={this.addUser}>
-						Invite
-					</Button>
-				</form>
-				<Dialog
-					open={this.state.modal}
-					TransitionComponent={Transition}
-					keepMounted
-					onClose={this.clearMessage}
-					aria-labelledby="alert-dialog-slide-title"
-					aria-describedby="alert-dialog-slide-description"
-				>
-					<DialogTitle id="alert-dialog-slide-title">{this.state.message}</DialogTitle>
+        if (this.state.users.length > 0) {
+          this.setState({ isLoading: false });
+        }
+      })
+      .catch(err => console.log(err));
+  }
 
-					<Button onClick={() => this.clearMessage()}>x</Button>
-				</Dialog>
-			</div>
-		);
-	}
-};
+  addUser = e => {
+    e.preventDefault();
+    //create mailObject to post to sendgrid API
+    const mailObject = {
+      email: this.state.newMemberEmail,
+      joinCode: this.state.joinCode
+    };
+    //sendgrid endpoint on our back end
+    const endpoint = `${baseURL}/email`;
+
+    axiosWithAuth()
+      .post(endpoint, mailObject)
+      .then(res => {
+        this.setState({ message: "Email sent!", modal: true });
+      })
+      .catch(err => {
+        this.setState({
+          message:
+            "There was an issue sending the email, please email your new team member manually.",
+          modal: true
+        });
+      });
+  };
+
+  changeHandler = e => {
+    this.setState({ newMemberEmail: e.target.value });
+  };
+  clearMessage = () => {
+    this.setState({ message: "", modal: false });
+  };
+
+  render() {
+    return (
+      <div className="inviteUserBox">
+        <div className="invite">Invite a new team member</div>
+        <form onSubmit={this.addUser} className="inviteUser">
+          <Input
+            id="outlined-email-input"
+            label="Email"
+            type="email"
+            onChange={this.changeHandler}
+            name="newMemberEmail"
+            autoComplete="email"
+            margin="normal"
+            variant="outlined"
+            placeholder="add email"
+          />
+          <br />
+          <Button variant="outlined" type="submit" onClick={this.addUser}>
+            Invite
+          </Button>
+        </form>
+        <Dialog
+          open={this.state.modal}
+          TransitionComponent={Transition}
+          keepMounted
+          onClose={this.clearMessage}
+          aria-labelledby="alert-dialog-slide-title"
+          aria-describedby="alert-dialog-slide-description"
+        >
+          <DialogTitle id="alert-dialog-slide-title">
+            {this.state.message}
+          </DialogTitle>
+
+          <Button onClick={() => this.clearMessage()}>x</Button>
+        </Dialog>
+      </div>
+    );
+  }
+}
 
 export default InviteUser;
