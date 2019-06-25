636054ab847f336781231981973e6e0830498ccd
Erica <eyufanchen@gmail.com>
2019-06-25 10:27:22 -0700

    fixed issue with circle graph not scaling right

--- client/src/components/circleProgress.js
+++ client/src/components/circleProgress.js
@@ -31,15 +31,17 @@ class circleProgress extends React.Component {
         return (
             <div className="circle-graph">
                 <div className="title">{this.props.title}</div>
-                <div className = "circle" id="circle"/>
-                <div className = "circle-background"></div>
-                <div className = "text-description">
-                    <div className = "percent-complete">
-                        <div className = "number">{numericPercent}</div>
-                        <div className = "percent">%</div>
-                    </div>
-                    <div className = "description">Completed</div>
-                    <div className = "today-date">{today}</div>
+                <div className ="circle-graph-contents">
+                    <div className = "circle" id="circle"/>
+                    <div className = "circle-background"></div>
+                    <div className = "text-description">
+                        <div className = "percent-complete">
+                            <div className = "number">{numericPercent}</div>
+                            <div className = "percent">%</div>
+                        </div>
+                        <div className = "description">Completed</div>
+                        <div className = "today-date">{today}</div>
+                </div>
                 </div>
                 
             </div>
