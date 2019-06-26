0ac24bbdd7c4460ff771a7c3d60dfe0904f1807a
curtis <chubbard022@gmail.com>
2019-06-26 10:38:11 -0700

    making mobile survey button connect to the reports page

--- client/src/pages/Navigation/Navigations.js
+++ client/src/pages/Navigation/Navigations.js
@@ -84,7 +84,12 @@ class Navigation extends React.Component {
           </div>
 
           <div className="horizontal-buttons mobile-add-poll">
-          <svg onClick={token.roles!=='admin' ? this.slackAuthCheck: null}className='menu-bar-add-poll-mobile' xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="none" d="M0 0h24v24H0V0z"/><path d="M13 7h-2v4H7v2h4v4h2v-4h4v-2h-4V7zm-1-5C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z"/></svg>
+
+          <NavLink to="/slackr/dashboard/reports/new">
+            <svg onClick={token.roles!=='admin' ? this.slackAuthCheck: null}className='menu-bar-add-poll-mobile' xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+               <path fill="none" d="M0 0h24v24H0V0z"/><path d="M13 7h-2v4H7v2h4v4h2v-4h4v-2h-4V7zm-1-5C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z"/>
+            </svg>
+          </NavLink>
 
           <div className = "add-to-slack"><SlackButton/></div>
           </div>
@@ -143,23 +148,6 @@ class Navigation extends React.Component {
 
                   </NavLink>
                 ) : null}
-
-              {/* {token.roles == "admin" ? (
-              <NavLink to="/slackr/dashboard/reports/new">
-                  <button
-                  className="menu-bar-add-poll"
-                  onClick={token.roles !== "admin" ? this.slackAuthCheck : null}
-                >
-                  +
-
-                  <div className="button-label-survey">
-                  Survey
-                </div>
-                
-                </button>
-              </NavLink>
-
-            ) : null} */}
 
 
                 <NavLink to="/slackr/dashboard">
