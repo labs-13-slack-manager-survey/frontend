3cc957e172073313a9bac2812d7bfebaeccf98f3
Ben <cbtsao47@gmail.com>
2019-06-26 15:29:24 -0500

    add mui picker

--- client/src/index.js
+++ client/src/index.js
@@ -6,13 +6,13 @@ import "./index.css";
 import "intro.js/introjs.css";
 import App from "./App";
 
-// import DateFnsUtils from '@date-io/date-fns';
+import DateFnsUtils from "@date-io/date-fns";
 
 ReactDOM.render(
   <Router>
-    {/* <MuiPickersUtilsProvider utils={DateFnsUtils}> */}
-    <App />
-    {/* </MuiPickersUtilsProvider> */}
+    <MuiPickersUtilsProvider utils={DateFnsUtils}>
+      <App />
+    </MuiPickersUtilsProvider>
   </Router>,
   document.getElementById("root")
 );
