175ae3cd362605d947714d55972f8471c9621424
Ben <cbtsao47@gmail.com>
2019-06-26 13:46:20 -0500

    remove unnecessary imports

--- client/src/components/FeedBackForm/Confirm.js
+++ client/src/components/FeedBackForm/Confirm.js
@@ -1,9 +1,8 @@
-import React, { Component } from "react";
+import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemText from "@material-ui/core/ListItemText";
-import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
 import withCardStyles from "./withCardStyles";
 
