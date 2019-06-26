b52ab81569d35d64809c2b2b974b87fbf48a9423
Erica <eyufanchen@gmail.com>
2019-06-26 12:31:24 -0700

    Added in an error message that will pop up when you try to filter a report for a day without any report results in it

--- client/src/components/PollCalendar.js
+++ client/src/components/PollCalendar.js
@@ -3,16 +3,24 @@ import "react-dates/initialize";
 import "react-dates/lib/css/_datepicker.css";
 import { DayPickerSingleDateController } from "react-dates";
 import "./pollCalendar.css";
+import PropTypes from 'prop-types';
 // import { isSameDay } from "date-fns";
 // import parseISO from "date-fns/parseISO";
 
 class PollCalendar extends Component {
+
+  isBlocked = day => {
+    const availableDates = ["2019-06-21", "2019-02-04"]
+    return !availableDates.some(date => day.isSame(date), 'day')
+  }
+
   render() {
     return (
       <div className="calendar-card">
         <div style={{ marginTop: "60px" }}>
           <div className="report-results-date-picker">
-            <DayPickerSingleDateController numberOfMonths={1} noBorder={true} />
+            <DayPickerSingleDateController numberOfMonths={1} noBorder={true}
+            isDayBlocked = {this.isBlocked} />
           </div>
         </div>
       </div>
